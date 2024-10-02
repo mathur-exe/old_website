3d1ef131a81d77c19e9b4cd9b4816edcd28ec867
mathur-exe <gmathur1993@gmail.com>
2024-10-02 16:13:48 +0530

    Quartz sync: Oct 2, 2024, 4:13 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://mathur-exe.github.io/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
